18125740415a278f086c49148e6d4d895977711c
Vanley Basso <vanley_basso@hotmail.com>
2025-02-14 13:38:33 -0300

    adjustments-form.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Form } from "./components/Form/Form";
 export const App = () => {
   return (
     <div className="bg-zinc-100 min-h-screen w-full flex flex-col items-center gap-6 pt-8">
-      <h1 className="font-bold text-3xl">React Hook Form Com TypeScript</h1>
+      <h1 className="font-bold text-3xl">Preencha o formulário abaixo</h1>
       <Form />
     </div>
   );
